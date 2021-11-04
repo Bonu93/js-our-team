0c25bd94dad1a568af3a2197acca7b83f09f9869
Bonu93 <luca.morucci.kb@gmail.com>
2021-11-04 14:54:46 +0100

    add members

--- main.js
+++ main.js
@@ -5,4 +5,62 @@ Ricreiamo le card del nostro team aggiungendo al layout di base fornito (allegat
 - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
 */
 
-console.log('JS OK');
+// members (objects) array 
+const members = [
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO', 
+        image : './img/wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        name : 'Angela Caroll',
+        role : 'Chief Editor', 
+        image : './img/angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager', 
+        image : './img/walter-gordon-office-manager.jpg',
+    },
+
+    {
+        name : 'Angela Lopez',
+        role : 'Social Media Manager', 
+        image : './img/angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        name : 'Scott Estrada',
+        role : 'Developer', 
+        image : './img/scott-estrada-developer.jpg',
+    },
+
+    {
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer', 
+        image : './img/barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+// dom refs 
+const teamContainer = document.querySelector('.team-container');
+
+
+for (let key in members) {
+    let member = members[key];
+    teamContainer.innerHTML += `
+    <div class="team-card">
+    <div class="card-image">
+      <img
+        src="${member.image}"
+        alt="${member.name}"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${member.name}</h3>
+      <p>${member.role}</p>
+    </div>
+  </div>`
+}
